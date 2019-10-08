5378fd00068b93e6364330f3844f0c4b6f8785cd
Madeline <mmcintosh2k14@gmail.com>
2019-10-08 18:42:40 -0400

    adds special, operator, and number button components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
+
+// STEP 3 - import the button and display components
+// imports button components:
+import Numbers from "../src/components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "../src/components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "../src/components/ButtonComponents/SpecialButtons/Specials";
+
+// // imports display components:
+// import Display from "./components/DisplayComponents/Display";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+
+// Don't forget to import any extra css/scss files you build into the correct component
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -18,6 +28,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        {/* <Display /> */}
+        <Numbers />
+        <Operators />
+        <Specials />
       </div>
     </div>
   );
