b5cc69179b503ad7cd80fa01635b2a69e2a4f38e
Madeline <mmcintosh2k14@gmail.com>
2019-10-09 12:01:51 -0400

    adds more styling to app buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -13,14 +13,14 @@ const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberButton, setNumberButton] = useState(numbers);
   return (
-    <div>
+    <>
       {/* // STEP 3 - Use .map() to iterate over your array data and return a button
       //  component matching the name on the provided file. Pass
       //  it any props needed by the child component */}
        {numberButton.map((button) => (
          <NumberButton  integer={button} />
        ))}
-    </div>
+    </>
   );
 };
 
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -10,14 +10,14 @@ const Specials = () => {
   const [specButton, setSpecButton] = useState(specials);
 
   return (
-    <div>
+    <>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
        {specButton.map((button) => (
          <SpecialButton  specs={button} />
          ))}
-    </div>
+    </>
   );
 };
 
